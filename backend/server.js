9132c113a7640e0199223fc0861a0d4259ff3877
Prajwal2246 <janbandhuprajwal28@gmail.com>
2026-01-17 21:56:06 +0530

    vercel changes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: true,
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -19,7 +19,8 @@ app.use(express.json());
 app.use("/api/quiz", quizRoutes);
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log("server running", { PORT }));
+}
 
 module.exports = app;
